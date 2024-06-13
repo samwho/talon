496e674185af4844e908d5bc25a348802ccda21c
Sam Rose <hello@samwho.dev>
2024-06-13 21:12:03 +0100

    Add correction commands.

--- samwho.py
+++ samwho.py
@@ -1,6 +1,6 @@
 import os
 
-from talon import Module
+from talon import Module, actions
 
 mod = Module()
 
@@ -12,3 +12,50 @@ class Actions:
         return os.system(
             'open "obsidian://advanced-uri?vault=Universe&daily=true&mode=append"'
         )
+
+    def correct(phrase: list[str]):
+        """Correct a word"""
+        idx = actions.user.find_phrase(phrase)
+        if not idx:
+            return
+
+        actions.edit.select_all()
+        actions.key("left")
+        for _ in range(idx):
+            actions.key("right")
+
+        for _ in range(len(" ".join(phrase))):
+            actions.edit.extend_right()
+
+        return idx
+
+    def selectXY(start: list[str], end: list[str]):
+        """Select from start to end"""
+        start_idx = actions.user.find_phrase(start)
+        if start_idx is None:
+            return
+
+        end_idx = actions.user.find_phrase(end)
+        if end_idx is None:
+            return
+
+        end_idx += len(" ".join(end))
+
+        actions.edit.select_all()
+        actions.key("left")
+        for _ in range(start_idx):
+            actions.key("right")
+
+        for _ in range(end_idx - start_idx):
+            actions.edit.extend_right()
+
+    def find_phrase(phrase: list[str]) -> int | None:
+        """Find a word"""
+        actions.edit.select_all()
+        text = actions.edit.selected_text()
+        actions.edit.select_none()
+
+        idx = text.lower().find(" ".join(phrase).lower())
+        if idx == -1:
+            return None
+        return idx
